fb4d35fa429b98b1c643c19b223e9367642ca43d
amir mohammad ghazanfari <amghira1@gmail.com>
2025-05-03 20:54:28 +0330

    Base Structure by TDD Added

---
+++ src/test/java/TicketPurchaseTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+public class TicketPurchaseTest {
+
+    @Test
+    void SuccessfulPurchase() {
+
+    }
+
+    @Test
+    void MovieIsNotExist() {
+
+    }
+
+    @Test
+    void TicketIsMoreThanAmount() {
+
+    }
+
+    @Test
+    void AmountDecreaseAfterSuccessfulPurchase() {
+
+    }
+
+    @Test
+    void CalculateCorrectPurchase() {
+
+    }
+
+}
